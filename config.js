9246fc0b1cb2116f6c70f6d5a5e07560335bf1f4
Mr.M1r4 <181756781+Mr-M1r4@users.noreply.github.com>
2026-09-11 23:19:17 -0500

    Configura URL del Web App de Apps Script

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ window.CLUB_VOLEIBOL = {
   // Ej: https://script.google.com/macros/s/AKfycb.../exec
   // Paso 1: despliega el proyecto en Apps Script (ver README.md).
   // Paso 2: pega aquí la URL que te arroja la publicación.
-  appUrl: ""
+  appUrl: "https://script.google.com/macros/s/AKfycbxEUvBGKvw2NYcIKckxnS-KCoKMet2aeKDusHi4Dcb60ELFBvT4Tzd3l6tSvpcmF4oF/exec"
 };
